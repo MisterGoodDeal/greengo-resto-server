35d40c9f5184af95b7a4fc3bb64b5a145bcceebb
Milan Camus <pro@turtletv.fr>
2022-06-16 15:42:29 +0200

    feat: ✏️ Set group code from 10 to 6 digits

--- src/utils/groups/utils.ts
+++ src/utils/groups/utils.ts
@@ -2,7 +2,7 @@
 export const generateGroupKey = () => {
   let text = "";
   const possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
-  for (let i = 0; i < 10; i++) {
+  for (let i = 0; i < 6; i++) {
     text += possible.charAt(Math.floor(Math.random() * possible.length));
   }
   return text;
