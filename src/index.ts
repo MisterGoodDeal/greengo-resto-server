821338de49468e492bc628141f756d148bc94bd9
Milan Camus <pro@turtletv.fr>
2022-06-19 12:31:54 +0200

    feat: ✨ Add favicon

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,15 @@
 import { getRoutes } from "./services/routes";
 import { webServices } from "./services/web";
 import { Request, Response } from "express";
+import path from "path";
 
 const env = require("dotenv").config();
 
 const express = require("express");
 const bodyParser = require("body-parser");
 const app = express();
+
+var favicon = require("serve-favicon");
 
 app.use(express.static(__dirname, { dotfiles: "allow" }));
 app.use(bodyParser.urlencoded({ limit: "50mb", extended: true }));
@@ -19,6 +22,7 @@ app.use((req: Request, res: Response, next: any) => {
   res.append("Access-Control-Allow-Headers", "*");
   next();
 });
+app.use(favicon(path.join(__dirname, "images", "favicon.ico")));
 
 app.get("/", function (req: Request, res: Response) {
   res.status(200).json({ "serial-luncher": { version: process.env.VERSION } });
