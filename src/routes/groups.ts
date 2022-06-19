e70123f7dc6fffa574a59a6f6fc4bfb7cf3177e6
Milan Camus <pro@turtletv.fr>
2022-06-19 12:31:31 +0200

    feat: ✨ Add group informations route

--- src/routes/groups.ts
+++ src/routes/groups.ts
@@ -6,11 +6,85 @@ import { images } from "../helpers/images.helpers";
 import { auth } from "../middleware/auth";
 import { groupReturnCode } from "../utils/groups/returnCodes";
 import { generateGroupKey } from "../utils/groups/utils";
-import { Group, UserGroupAssociation } from "../utils/groups/interfaces";
+import {
+  Group,
+  GroupInfo,
+  UserGroupAssociation,
+} from "../utils/groups/interfaces";
+import { User } from "../utils/user/interfaces";
+import { Place } from "../utils/places/interfaces";
 
 const env = require("dotenv").config();
 
 const groups = (app: any) => {
+  // Get GroupInfo
+  app.get(
+    "/group/info/:group_id",
+    auth,
+    async function (req: Request, res: Response) {
+      // @ts-ignore
+      const user: JWTProps = req.user;
+
+      const { group_id } = req.params;
+      const group: Group[] = await db.queryParams(
+        "SELECT * FROM LunchGroups WHERE id = ?",
+        [group_id]
+      );
+      if (group.length === 0) {
+        res
+          .status(groupReturnCode.groupNotFound.code)
+          .json(groupReturnCode.groupNotFound.payload);
+      } else {
+        const assoc: UserGroupAssociation[] = await db.queryParams(
+          "SELECT * FROM UsersLauchGroupsAssoc WHERE fk_user = ? AND fk_lunch_group = ?",
+          [user.id, group_id]
+        );
+        if (assoc.length === 0) {
+          res
+            .status(groupReturnCode.notInGroup.code)
+            .json(groupReturnCode.notInGroup.payload);
+        } else {
+          const finalGroup: GroupInfo = {
+            group: group[0],
+            users: [],
+            last_places: [],
+          };
+
+          // Get all users from the group
+          const users: User[] = await db.queryParams(
+            "SELECT * FROM Users WHERE id IN (SELECT fk_user FROM UsersLauchGroupsAssoc WHERE fk_lunch_group = ?)",
+            [group_id]
+          );
+          users.map((u) => {
+            finalGroup.users.push({
+              firstname: u.firstname,
+              lastname: u.lastname,
+              profile_picture: u.profile_picture,
+            });
+          });
+
+          // Get 5 last places from the group
+          const places: Place[] = await db.queryParams(
+            "SELECT * FROM LunchPlaces WHERE id IN (SELECT id FROM LunchPlaces WHERE fk_lunch_group = ?) ORDER BY created_at DESC LIMIT 5",
+            [group_id]
+          );
+          places.map((p) => {
+            finalGroup.last_places.push({
+              name: p.name,
+              country_speciality: p.country_speciality,
+              rating: p.rating,
+              price_range: p.price_range,
+              image: p.image,
+              can_bring_reusable_content: p.can_bring_reusable_content,
+            });
+          });
+
+          res.status(200).json(finalGroup);
+        }
+      }
+    }
+  );
+
   // Create a new group
   app.post("/group/create", auth, async function (req: Request, res: Response) {
     // @ts-ignore
@@ -21,31 +95,32 @@ const groups = (app: any) => {
         .status(returnCode.missingParameters.code)
         .json(returnCode.missingParameters.payload);
     } else {
-      const isGroupExists = await db.queryParams(
-        "SELECT * FROM LunchGroups WHERE fk_user = ?",
-        [user.id]
+      const imgurImg = await images.upload(image);
+      const group: MySQLResponse = await db.queryParams(
+        "INSERT INTO LunchGroups (name, image, fk_user, group_key) VALUES (?, ?, ?, ?)",
+        [name, imgurImg.data.link, user.id, generateGroupKey()]
       );
-      if (isGroupExists.length > 0) {
-        res
-          .status(groupReturnCode.groupAlreadyExists.code)
-          .json(groupReturnCode.groupAlreadyExists.payload);
-      } else {
-        const imgurImg = await images.upload(image);
-        const group: MySQLResponse = await db.queryParams(
-          "INSERT INTO LunchGroups (name, image, fk_user, group_key) VALUES (?, ?, ?, ?)",
-          [name, imgurImg.data.link, user.id, generateGroupKey()]
+      if (group.affectedRows > 0) {
+        const newGroup = await db.queryParams(
+          "SELECT * FROM LunchGroups WHERE id = ?",
+          [group.insertId]
         );
-        if (group.affectedRows > 0) {
-          const newGroup = await db.queryParams(
-            "SELECT * FROM LunchGroups WHERE id = ?",
-            [group.insertId]
-          );
-          res.status(200).json(newGroup);
+        // join the created group
+        const userGroupAssociation: MySQLResponse = await db.queryParams(
+          "INSERT INTO UsersLauchGroupsAssoc (fk_user, fk_lunch_group) VALUES (?, ?)",
+          [user.id, newGroup[0].id]
+        );
+        if (userGroupAssociation.affectedRows > 0) {
+          res.status(200).json(newGroup[0]);
         } else {
           res
-            .status(returnCode.internalError.code)
-            .json(returnCode.internalError.payload);
+            .status(returnCode.unknownUser.code)
+            .json(returnCode.unknownUser.payload);
         }
+      } else {
+        res
+          .status(returnCode.internalError.code)
+          .json(returnCode.internalError.payload);
       }
     }
   });
@@ -57,6 +132,7 @@ const groups = (app: any) => {
     async function (req: Request, res: Response) {
       // @ts-ignore
       const user: JWTProps = req.user;
+
       const { group_key } = req.params;
       const group: Group[] = await db.queryParams(
         "SELECT * FROM LunchGroups WHERE group_key = ?",
@@ -99,6 +175,7 @@ const groups = (app: any) => {
     async function (req: Request, res: Response) {
       // @ts-ignore
       const user = req.user;
+
       const { group_id } = req.params;
       const assoc: UserGroupAssociation[] = await db.queryParams(
         "SELECT * FROM UsersLauchGroupsAssoc WHERE fk_user = ? AND fk_lunch_group = ?",
@@ -127,26 +204,22 @@ const groups = (app: any) => {
   );
 
   // Get group by group_key
-  app.get(
-    "/group/:group_id",
-    auth,
-    async function (req: Request, res: Response) {
-      // @ts-ignore
-      const user = req.user;
-      const { group_id } = req.params;
-      const group: Group[] = await db.queryParams(
-        "SELECT * FROM LunchGroups WHERE group_key = ?",
-        [group_id]
-      );
-      if (group.length === 0) {
-        res
-          .status(groupReturnCode.groupNotFound.code)
-          .json(groupReturnCode.groupNotFound.payload);
-      } else {
-        res.status(200).json(group[0]);
-      }
+  app.get("/group/:group_id", async function (req: Request, res: Response) {
+    // @ts-ignore
+    const user = req.user;
+    const { group_id } = req.params;
+    const group: Group[] = await db.queryParams(
+      "SELECT * FROM LunchGroups WHERE group_key = ?",
+      [group_id]
+    );
+    if (group.length === 0) {
+      res
+        .status(groupReturnCode.groupNotFound.code)
+        .json(groupReturnCode.groupNotFound.payload);
+    } else {
+      res.status(200).json(group[0]);
     }
-  );
+  });
 
   // Delete a group
   app.delete(
@@ -155,11 +228,13 @@ const groups = (app: any) => {
     async function (req: Request, res: Response) {
       // @ts-ignore
       const user = req.user;
+
       const { group_id } = req.params;
       const group: Group[] = await db.queryParams(
         "SELECT * FROM LunchGroups WHERE id = ?",
         [group_id]
       );
+
       if (group.length === 0 || group[0].fk_user !== user.id) {
         res
           .status(groupReturnCode.groupNotFound.code)
--- src/utils/groups/interfaces.ts
+++ src/utils/groups/interfaces.ts
@@ -14,3 +14,20 @@ export interface UserGroupAssociation {
   fk_user: number;
   fk_group: number;
 }
+
+export interface GroupInfo {
+  group: Partial<Group>;
+  users: {
+    firstname: string;
+    lastname: string;
+    profile_picture: string;
+  }[];
+  last_places: {
+    name: string;
+    country_speciality: string;
+    rating: number;
+    price_range: number;
+    image: string;
+    can_bring_reusable_content: boolean;
+  }[];
+}
