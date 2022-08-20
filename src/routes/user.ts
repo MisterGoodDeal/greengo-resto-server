22e9427897e8ee1227130c3c59d4dd30b67a904a
Milan Camus <pro@turtletv.fr>
2022-08-20 22:35:44 +0200

    feat: ✨ Edit user route

--- src/routes/user.ts
+++ src/routes/user.ts
@@ -22,18 +22,46 @@ const user = (app: any) => {
       "SELECT * FROM Users WHERE id = ?",
       [id]
     );
-    res.status(200).json({ query: userQueryBuilder(partialUser) });
-    // if (user.length === 0) {
-    //   const user: User = await db.queryParams(
-    //     "UPDATE Users SET ? WHERE id = ?",
-    //     [partialUser, id]
-    //   );
-    //   res.status(200).json(user);
-    // } else {
-    //   res
-    //     .status(returnCode.unknownUser.code)
-    //     .json(returnCode.unknownUser.payload);
-    // }
+    if (user.length === 1) {
+      if (partialUser.profile_picture) {
+        const imgurImage = partialUser.profile_picture
+          ? await images.upload(partialUser.profile_picture)
+          : { data: { link: "" } };
+        partialUser.profile_picture = imgurImage.data.link;
+      }
+
+      if (partialUser.password) {
+        partialUser.password = await pwdUtils.hash(partialUser.password);
+      }
+
+      // New date to format (YYYY-MM-DD HH:MM:SS)
+      const date = new Date();
+      const year = date.getFullYear();
+      const month = date.getMonth() + 1;
+      const day = date.getDate();
+      const hours = date.getHours();
+      const minutes = date.getMinutes();
+      const seconds = date.getSeconds();
+      const formattedDate = `${year}-${month}-${day} ${hours}:${minutes}:${seconds}`;
+
+      // @ts-ignore
+      partialUser.updated_at = formattedDate;
+
+      const query = userQueryBuilder(partialUser);
+      console.log(query);
+
+      const user: MySQLResponse = await db.queryParams(
+        `UPDATE Users SET ${query} WHERE id = ?`,
+        [id]
+      );
+      console.log(user);
+
+      res.status(200).json(user);
+    } else {
+      res
+        .status(returnCode.unknownUser.code)
+        .json(returnCode.unknownUser.payload);
+    }
   });
 };
 
