2830013a716dd470e31ca1ee4ff5bf024d0f579e
Milan Camus <pro@turtletv.fr>
2022-10-11 15:46:04 +0200

    chore: 🗑 Remove test route

--- src/index.ts
+++ src/index.ts
@@ -34,21 +34,6 @@ app.get("/cgu", function (req: Request, res: Response) {
   res.sendFile(path.join(__dirname, "cgu.html"));
 });
 
-app.get("/testNotification/:token", function (req: Request, res: Response) {
-  const token = req.params.token;
-  try {
-    notification.send([token], {
-      title: "Test",
-      body: "Test body from server",
-      extra: "some extra data",
-    });
-    res.status(200).json({ testNotification: true });
-  } catch (error) {
-    console.log(error);
-    res.status(500).json({ testNotification: false, error });
-  }
-});
-
 const routes = getRoutes();
 routes.forEach((route: string) => {
   import("./routes/" + route).then((r: any) => {
