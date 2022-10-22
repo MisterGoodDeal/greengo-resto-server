9dea735596687e8bb1a3528731781546ecfd03f5
Milan Camus <pro@turtletv.fr>
2022-10-22 02:21:23 +0200

    feat: ✨ Implement routing routes

--- src/middleware/auth.ts
+++ src/middleware/auth.ts
@@ -1,3 +1,4 @@
+import { logger } from "../services/logger.service";
 import { verifyJwt } from "../utils/jwt";
 import { returnCode } from "../utils/returnCodes";
 
@@ -5,6 +6,10 @@ export const auth = (req: any, res: any, next: any) => {
   const token = req.headers["x-auth"];
 
   if (!token) {
+    logger.webhook({
+      message: `Unauthorized request from ${req.ip} using the token ${token}`,
+      type: "warning",
+    });
     return res
       .status(returnCode.missingToken.code)
       .send(returnCode.missingToken.payload);
